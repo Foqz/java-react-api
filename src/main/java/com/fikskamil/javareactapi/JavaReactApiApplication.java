e464b1c044578436dfeea13897c3331c9e4a41b6
Kamil Fiks <fikskamil@gmail.com>
2024-06-15 17:46:38 +0200

    Innitial commit

---
+++ src/main/java/com/fikskamil/javareactapi/JavaReactApiApplication.java
@@ -0,0 +1,13 @@
+package com.fikskamil.javareactapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaReactApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fikskamil/javareactapi/JavaReactApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fikskamil.javareactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReactApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
